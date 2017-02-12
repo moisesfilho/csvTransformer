20a34d1f76ececa8ac6c843d2c1687a13f4675d0
Moisés Filho <moises.son@gmail.com>
2017-02-12 11:24:06 -0300

    Correção de refatoração e ajuste na validação

--- csvTransformer/Controllers/ConverterController.cs
+++ csvTransformer/Controllers/ConverterController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using System.Web.Mvc;
 using csvTransformer.Container;
 using csvTransformer.Models.Business;
@@ -19,12 +20,15 @@ namespace csvTransformer.Controllers
             _converter = converter;
         }
 
-        public ActionResult XlcToCsv()
+        [HttpGet]
+        public ActionResult XlsToCsv()
         {
             return View();
         }
         
-        public ActionResult Upload()
+        [HttpPost]
+        [ActionName("XlsToCsv")]
+        public ActionResult Execute()
         {
             try
             {
@@ -39,19 +43,19 @@ namespace csvTransformer.Controllers
                 if (file == null || file.ContentLength <= 0)
                     throw new Exception("Arquivo com conteudo inválido.");
 
-                var extensoesValidas = new List<string> {"xls", "xlsx"};
+                var extensoesValidas = new List<string> {".xls", ".xlsx"};
 
                 if (!extensoesValidas.Contains(Path.GetExtension(file.FileName)))
                     throw new Exception("Arquivo com extensão diferente de XLS ou XLSX.");
 
                 var result = _converter.File(file.InputStream).To(FileType.csv).From(FileType.xls).Result();
 
-                return File(result, System.Net.Mime.MediaTypeNames.Application.Octet, file.FileName.Replace("xlsx", "csv"));
+                return File(result, MediaTypeNames.Application.Octet, file.FileName.Replace("xlsx", "csv"));
             }
             catch (Exception exception)
             {
                 ModelState.AddModelError("Exception", exception.Message);
-                return View("Xls");
+                return View("XlsToCsv");
             }
         }
     }
